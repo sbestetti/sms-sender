6a6ca6c9061b0dbc0113507efa1f0622facecae4
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-10-29 10:37:00 +0000

    Running through file

--- app/runner.py
+++ app/runner.py
@@ -7,5 +7,7 @@ def process_file(filename):
         settings.UPLOAD_FOLDER,
         filename
     )
-
-    print(file_path)
+    with open(file_path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            print(row[0])
