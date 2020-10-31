e088bce365245d69f826c23fad311784e2f932db
sbestetti <sbestetti@gmail.com>
2020-10-31 12:19:11 +0000

    New CSV format

--- app/tools.py
+++ app/tools.py
@@ -1,17 +1,20 @@
-import os, csv
+import csv
+import os
 
 from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
 
 import settings
 
 client = Client(settings.SID, settings.TOKEN)
 
+
 def check_numbers(numbers):
     numbers_not_found = list()
     for number in numbers:
         try:
-            phone_number = client.lookups.phone_numbers(number[0]).fetch()
-        except Exception as e:
+            client.lookups.phone_numbers(number[1]).fetch()
+        except TwilioRestException:
             numbers_not_found.append(number)
     return numbers_not_found
 
@@ -34,10 +37,14 @@ def send_messages(number_list):
     flag = 0
     while flag < len(number_list):
         message = client.messages.create(
-            body=number_list[flag][1],
-            from_="APITest",
-            to=number_list[flag][0]
+            body=number_list[flag][2],
+            from_=number_list[flag][0],
+            to=number_list[flag][1]
         )
+        number_list[flag].append(message.status)
         number_list[flag].append(message.sid)
         flag += 1
+    for item in number_list:
+        current_message = client.messages.get(item[4]).fetch()
+        item[3] = current_message.status
     return number_list
